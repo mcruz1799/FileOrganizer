b01e9dd72d2651f80a1953c697f5c6011c8bfc75
Matthew Cruz <matthewcruz@Matthews-Air.lan>
2023-12-10 18:36:09 -0500

    corrected extensions to be case insensitive; added file by extensions without date

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def organize_files_by_date_and_extension(source_folder, destination_parent_folde
         source_path = os.path.join(source_folder, filename)
 
         # Check if it's a file (not a subdirectory) and has the specified extension
-        if os.path.isfile(source_path) and filename.endswith(extension):
+        if os.path.isfile(source_path) and filename.lower().endswith(extension.lower()):
 
             # Get the file's date
             file_date = get_file_date(source_path)
@@ -62,7 +62,7 @@ def organize_files_by_date_and_extension(source_folder, destination_parent_folde
             move_file(source_path, destination_path)
 
 
-def organize_files_by_name(source_folder, destination_parent_folder, keywords, folder_name):
+def organize_files_by_name(source_folder, destination_parent_folder, keywords, folder_name=""):
     """
     Organize files with any of the given words in the keywords array in their name into a folder named folder_name. 
     For example, keywords "resume, cover, resumes" into resumes
@@ -77,11 +77,35 @@ def organize_files_by_name(source_folder, destination_parent_folder, keywords, f
         # Check if it's a file (not a subdirectory) and if the name contains any of the specified keywords
         if os.path.isfile(source_path) and any(keyword.lower() in filename.lower() for keyword in keywords):
 
-            # Create the destination path based on the keyword
-            destination_path = os.path.join(destination_parent_folder, folder_name)
+            
+            if folder_name != "":
+                # Create the destination path based on the keyword
+                destination_path = os.path.join(destination_parent_folder, folder_name)
+            else:
+                destination_path = destination_parent_folder
 
             # Ensure the destination folder exists
             create_directory(destination_path)
+
+            # Move the file to the destination folder
+            move_file(source_path, destination_path)
+
+def organize_files_by_extensions(source_folder, destination_folder, extensions):
+    """
+    Moves files with any of the specified extensions to the destination folder. 
+    """
+
+    # Iterate through files in the source folder
+    for filename in os.listdir(source_folder):
+        source_path = os.path.join(source_folder, filename)
+
+        # Check if it's a file (not a subdirectory) and has the specified extension
+        if os.path.isfile(source_path) and any(filename.lower().endswith(extension.lower()) for extension in extensions):
+
+            # Ensure the destination folder exists
+            create_directory(destination_folder)
+
+            destination_path = destination_folder
 
             # Move the file to the destination folder
             move_file(source_path, destination_path)
@@ -96,10 +120,33 @@ if __name__ == "__main__":
     source_folder = "/Volumes/Macintosh HD/Users/matthewcruz/Downloads"
     destination_parent_folder = "/Volumes/Seagate /MattCruzFitness/Fitness Vids/RawVideos"
 
-    # Organize files by extension (e.g., move all .txt files to the folder with a custom name including text and the current date)
     organize_files_by_date_and_extension(source_folder, destination_parent_folder, "RawVideoContent", ".mov")
 
 
     #### This will organize the resumes I have in my Downloads folder into one Resume folder
     source_folder = "/Volumes/Macintosh HD/Users/matthewcruz/Downloads"
     destination_parent_folder = "/Volumes/Macintosh HD/Users/matthewcruz/Documents"
+
+    organize_files_by_name(source_folder, destination_parent_folder, ["resume", "cover"],"Resumes")
+
+
+    #### This will organize any meal plans and guide into one Meal Guides folder
+    source_folder = "/Volumes/Macintosh HD/Users/matthewcruz/Downloads"
+    destination_parent_folder = "/Volumes/Seagate /MattCruzFitness"
+    organize_files_by_name(source_folder, destination_parent_folder, ["meal", "diet"],"MealGuides")
+
+    #### This will add any updated versions of my logo into a Logo folder
+    source_folder = "/Volumes/Macintosh HD/Users/matthewcruz/Downloads"
+    destination_parent_folder = "/Volumes/Seagate /MattCruzFitness"
+    organize_files_by_name(source_folder, destination_parent_folder, ["logo"],"Logos")
+
+    #### This will move b reel downloads to their appropriate folder
+    source_folder = "/Volumes/Macintosh HD/Users/matthewcruz/Downloads"
+    destination_parent_folder = "/Volumes/Seagate /MattCruzFitness/FitnessVids/b-reel"
+    organize_files_by_extensions(source_folder, destination_parent_folder, ["jpg","jpeg","gif","mp4"])
+
+
+    #### This will add any miscellaneous fitness files into an appropriate folder
+    source_folder = "/Volumes/Macintosh HD/Users/matthewcruz/Downloads"
+    destination_parent_folder = "/Volumes/Seagate /MattCruzFitness"
+    organize_files_by_name(source_folder, destination_parent_folder, ["fitness", "motivate","progressive", "testimonial", "muscle","belly","curvy","train","lift","strength","guide","tips"],"Misc")
