b8bf1f92ea7b975998bae2e056b164b502c79db7
Matthew Cruz <matthewcruz@Matthews-Air.lan>
2023-12-10 11:38:13 -0500

    added helped to get the date a file was created or modified

--- main.py
+++ main.py
@@ -10,8 +10,22 @@ def create_directory(directory_path):
 def move_file(source_path, destination_path):
     # Move the file to the destination folder
     shutil.move(source_path, destination_path)
+
+def get_file_date(file_path, mode='created'):
+    # Get the creation or modification date of the file
+    if mode == 'created':
+        timestamp = os.path.getctime(file_path)
+    elif mode == 'modified':
+        timestamp = os.path.getmtime(file_path)
+    else:
+        raise ValueError("Invalid mode. Use 'created' or 'modified'.")
+
+    return datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d")
     
 def organize_files_by_extension(source_folder, destination_parent_folder, folder_prefix, extension):
+    """Move files with the specified extension from the given source path to a new folder within 
+    the destination_parent folder (if it doesn't already exist). 
+    The new folder will be named {folder_prefix}_{current_date} for example 'RawVideoContent_Dec-10-2023'"""
     # Get the current date
     current_date = datetime.now().strftime("%b-%d-%Y")
 
@@ -35,11 +49,11 @@ def organize_files_by_extension(source_folder, destination_parent_folder, folder
 
             # Move the file to the destination folder
             move_file(source_path, destination_path)
-            
+
 if __name__ == "__main__":
     # Replace these paths with your actual source and parent destination folders
     source_folder = "/path/to/source/folder"
     destination_parent_folder = "/path/to/destination/parent/folder"
 
     # Organize files by extension (e.g., move all .txt files to the folder with a custom name including text and the current date)
-    organize_files_by_extension(source_folder, destination_parent_folder, ".txt")
+    organize_files_by_extension(source_folder, destination_parent_folder, "RawVideoContent", ".txt")
